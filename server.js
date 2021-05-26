6357b162e8b36e99fc900a502a4aea60bf732b63
Rony Braswell <ronybraswell@hotmail.com>
2021-05-26 19:26:43 -0400

    load dependencies and set up express app for listening

---
+++ server.js
@@ -0,0 +1,11 @@
+// load dependencies
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+
+//set up express app
+const app = express();
+
+//set port for listening
+const PORT = process.env.PORT || 3000;
+
